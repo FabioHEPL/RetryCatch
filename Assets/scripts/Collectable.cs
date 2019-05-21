586218897901e5392541f9e865a7d2a75d1f4abd
FabioHEPL <fabio.hepl15@gmail.com>
2019-05-21 15:09:01 +0200

    Gestion de l'inventaire + Première scene "prototype-1.unity"

---
+++ Assets/scripts/Collectable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public interface ICollectable
+{
+    int ItemID { get; }
+}
+
+public class Collectable : MonoBehaviour, ICollectable
+{
+    // -------------------------------------------------------------------------------------
+    // @ PUBLIC
+    // -------------------------------------------------------------------------------------
+    public int ItemID => _itemID;
+
+    // -------------------------------------------------------------------------------------
+    // @ PRIVATE
+    // -------------------------------------------------------------------------------------
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    [SerializeField]
+    private int _itemID = 0;
+}
