1710623255bed9303c51ed1a62380ad241380cfc
pludo <ludovic.hod@gmail.com>
2019-05-21 15:09:09 +0200

    implementation du Gun/Movement/projectile.cs

---
+++ Assets/scripts/projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+  
+    public float vitesseDeProjectile = 25.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 0.5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * vitesseDeProjectile * Time.deltaTime);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+}
