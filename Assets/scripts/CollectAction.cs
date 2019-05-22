5f7f1734016a6822f420e8ff22f6b2c08b61e142
pludo <ludovic.hod@gmail.com>
2019-05-22 17:49:41 +0200

    co;;it de gros degueulasse

---
+++ Assets/scripts/CollectAction.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IAction
+{
+    void Execute();
+}
+
+
+public class CollectAction : MonoBehaviour, IAction
+{
+    // -------------------------------------------------------------------------------------
+    // @ PUBLIC
+    // -------------------------------------------------------------------------------------
+    public void Execute()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(
+            new Vector2(transform.position.x, transform.position.y),
+            _radius);
+
+        Collectable collectable = collider?.GetComponent<Collectable>();        
+
+        if (collectable != null)
+        {
+            _inventory.AddElement(collectable.ItemID);            
+            Destroy(collectable.gameObject);
+        }
+    }
+
+    // -------------------------------------------------------------------------------------
+    // @ PRIVATE
+    // -------------------------------------------------------------------------------------
+    private void Awake()
+    {       
+        _inventory = GetComponent<Inventory>();
+        if (_inventory == null)
+        {
+            Debug.Log("Cannot collect items if there is no inventory.");
+            enabled = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Execute();
+        }
+    }
+
+    [SerializeField]
+    private float _radius = 5f;
+    [SerializeField]
+    private Inventory _inventory;
+}
---
+++ Assets/scripts/Gun.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Weapon : MonoBehaviour
+{
+    public abstract void Fire(Vector2 direction);
+}
+
+public class Gun : Weapon
+{
+
+    public float fireDistance = 2.0f;
+    public GameObject Bullet;
+    public float speed = 20.0f;
+    public override void Fire(Vector2 direction)
+    {
+        direction.Normalize();
+        var recuperationPSourisDegee = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+
+        Instantiate(Bullet, transform.position + new Vector3(direction.x, direction.y, 0.0f) * fireDistance, Quaternion.Euler(0.0f, 0.0f, recuperationPSourisDegee));
+        Debug.Log(direction);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool myFire = Input.GetButtonDown("Fire1");
+        if (myFire == true)
+        {
+            Vector2 positionSouris = recuperationPSouris();
+            Vector2 positionActuelle = (Vector2)transform.position;
+
+
+            Fire(positionSouris - positionActuelle);
+        }
+
+    }
+
+    public Vector2 recuperationPSouris()
+    {
+        //var cam = ;       
+        Vector3 mousePos = Input.mousePosition;
+        //mousePos.x = currentEvent.mousePosition.x;
+        //mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        Vector3 point = Camera.main.ScreenToWorldPoint(mousePos);
+        var positionMouse2D = (Vector2)point;
+
+        return positionMouse2D;
+    }
+
+
+}
